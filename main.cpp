914f1b9419b72103696e9d84d3d216c6370f92ea
timter <ttproduced@gmail.com>
2025-03-18 10:06:18 +0300

    Laba 2.2

--- main.cpp
+++ main.cpp
@@ -2,43 +2,58 @@
 #include <tchar.h> // for text
 #include <vector>
 
-struct Shape {
-    enum Type { CIRCLE, RECTANGLE } type;
-    RECT rect; // winapi struct, rectangle
-
-    Shape(Type t, int left, int top, int right, int bottom) : type(t), rect{left, top, right, bottom} {}
+class Shape {
+public:
+    RECT rect;
+    Shape(int left, int top, int right, int bottom) : rect{left, top, right, bottom} {}
+    virtual void Draw(HDC hdc) const = 0;
+    virtual ~Shape() {}
 };
 
-std::vector<Shape> shapes;
+class RectangleShape : public Shape {
+public:
+    RectangleShape(int left, int top, int right, int bottom) : Shape(left, top, right, bottom) {}
+    void Draw(HDC hdc) const override {
+        Rectangle(hdc, rect.left, rect.top, rect.right, rect.bottom);
+    }
+};
+
+class EllipseShape : public Shape {
+public:
+    EllipseShape(int left, int top, int right, int bottom) : Shape(left, top, right, bottom) {}
+    void Draw(HDC hdc) const override {
+        Ellipse(hdc, rect.left, rect.top, rect.right, rect.bottom);
+    }
+};
+
+std::vector<Shape*> shapes;
 
 // обработчик сообщений
-// дескрипитор окна, код события, доп данные для события, доп данные
+// дескриптор окна, код события, доп данные для события, доп данные
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
     switch (message) {
     case WM_CREATE: { // создание окна
-        shapes.emplace_back(Shape::CIRCLE, 50, 50, 150, 150);
-        shapes.emplace_back(Shape::RECTANGLE, 200, 50, 300, 150);
+        shapes.push_back(new EllipseShape(50, 50, 150, 150));
+        shapes.push_back(new RectangleShape(200, 50, 300, 150));
         break;
     }
     case WM_PAINT: { // перерисовка окна
         PAINTSTRUCT ps;
-        HDC hdc = BeginPaint(hWnd, &ps); // процесс рисования
-
-        for (std::vector<Shape>::iterator it = shapes.begin(); it != shapes.end(); it++) {
-            if (it->type == Shape::CIRCLE) {
-                Ellipse(hdc, it->rect.left, it->rect.top, it->rect.right, it->rect.bottom); // окр вписанная в прямоугольник
-            } else if (it->type == Shape::RECTANGLE) {
-                Rectangle(hdc, it->rect.left, it->rect.top, it->rect.right, it->rect.bottom);
-            }
+        HDC hdc = BeginPaint(hWnd, &ps);
+        for (size_t i = 0; i < shapes.size(); ++i) {
+            shapes[i]->Draw(hdc);
         }
-
         EndPaint(hWnd, &ps);
         break;
     }
-    case WM_DESTROY: // закрытие окна
+    case WM_DESTROY:
+        for (size_t i = 0; i < shapes.size(); ++i) {
+            delete shapes[i];
+        }
+        shapes.clear();
         PostQuitMessage(0);
         break;
-    default: // стандартный обработчик windows
+    default:
         return DefWindowProc(hWnd, message, wParam, lParam);
     }
     return 0;
@@ -50,11 +65,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     wc.lpfnWndProc = WndProc; // указ функцию обработки сообщений
     wc.hInstance = hInstance; // передача дескриптора программы
     wc.lpszClassName = _T("Shapes");
-
     RegisterClass(&wc); // регистрация окна в системе
 
     HWND hWnd = CreateWindow(wc.lpszClassName, _T("Shapes"), WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT, 400, 300, nullptr, nullptr, hInstance, nullptr);
-
     if (!hWnd) return 0;
 
     ShowWindow(hWnd, nCmdShow);
@@ -65,6 +78,5 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
         TranslateMessage(&msg);
         DispatchMessage(&msg); // отправляет сообщения в wndProc()
     }
-
     return static_cast<int>(msg.wParam);
 }
